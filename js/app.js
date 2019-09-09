c8324be467a33d2ca85df02ee833cff32a2a20cd
dwu68 <dongli.pzh@gmail.com>
2019-09-09 15:52:23 -0400

    add check leap year

--- js/app.js
+++ js/app.js
@@ -12,3 +12,20 @@ function nameCheck(){
         document.getElementById("welcome_msg").innerHTML = "Welcome!";     
     }
 }
+
+function checkLeapYear() {
+    //clear for resubmits
+    document.getElementById("result").innerHTML = "";
+
+    var year = document.getElementById("year").value;
+    var answer = "Not a leap year"
+
+    if (year % 4 == 0) {
+        answer = "It's a leap year!";
+    }
+    if (year % 100 == 0) {
+        answer = "Not a leap year";
+    }
+    if (year % 100 == 0 && year % 400 == 0) {
+        answer = "It's a leap year!";
+    }
